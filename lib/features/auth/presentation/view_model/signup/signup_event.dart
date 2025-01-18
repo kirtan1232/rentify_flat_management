d8831b8ba0acd2e437fc5a4965f3028315595cdb
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-01-18 21:02:18 +0545

    signup

--- lib/features/auth/presentation/view_model/signup/signup_event.dart
+++ lib/features/auth/presentation/view_model/signup/signup_event.dart
@@ -6,3 +6,20 @@ sealed class SignupEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class SignupUser extends SignupEvent {
+  final BuildContext context;
+  final String fullName;
+
+  final String phone;
+  final String email;
+  final String password;
+
+  const SignupUser({
+    required this.context,
+    required this.fullName,
+    required this.phone,
+    required this.email,
+    required this.password,
+  });
+}
--- lib/features/auth/presentation/view_model/signup/signup_state.dart
+++ lib/features/auth/presentation/view_model/signup/signup_state.dart
@@ -1,10 +1,27 @@
 part of 'signup_bloc.dart';
 
-sealed class SignupState extends Equatable {
-  const SignupState();
-  
-  @override
-  List<Object> get props => [];
+class SignupState {
+  final bool isLoading;
+  final bool isSuccess;
+
+  SignupState({
+    required this.isLoading,
+    required this.isSuccess,
+  });
+
+  SignupState.initial()
+      : isLoading = false,
+        isSuccess = false;
+
+  SignupState copyWith({
+    bool? isLoading,
+    bool? isSuccess,
+  }) {
+    return SignupState(
+      isLoading: isLoading ?? this.isLoading,
+      isSuccess: isSuccess ?? this.isSuccess,
+    );
+  }
 }
 
-final class SignupInitial extends SignupState {}
+
