3b4008fb0953a1b395d74797bffdd81841e53805
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-01-18 21:02:07 +0545

    login

--- lib/features/auth/presentation/view_model/login/login_event.dart
+++ lib/features/auth/presentation/view_model/login/login_event.dart
@@ -6,3 +6,35 @@ sealed class LoginEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class NavigateRegisterScreenEvent extends LoginEvent {
+  final BuildContext context;
+  final Widget destination;
+
+  const NavigateRegisterScreenEvent({
+    required this.context,
+    required this.destination,
+  });
+}
+
+class NavigateHomeScreenEvent extends LoginEvent {
+  final BuildContext context;
+  final Widget destination;
+
+  const NavigateHomeScreenEvent({
+    required this.context,
+    required this.destination,
+  });
+}
+
+class LoginUserEvent extends LoginEvent {
+  final BuildContext context;
+  final String email;
+  final String password;
+
+  const LoginUserEvent({
+    required this.context,
+    required this.email,
+    required this.password,
+  });
+}
--- lib/features/auth/presentation/view_model/login/login_state.dart
+++ lib/features/auth/presentation/view_model/login/login_state.dart
@@ -1,10 +1,25 @@
 part of 'login_bloc.dart';
 
-sealed class LoginState extends Equatable {
-  const LoginState();
-  
-  @override
-  List<Object> get props => [];
-}
+class LoginState {
+  final bool isLoading;
+  final bool isSuccess;
 
-final class LoginInitial extends LoginState {}
+  LoginState({
+    required this.isLoading,
+    required this.isSuccess,
+  });
+
+  LoginState.initial()
+      : isLoading = false,
+        isSuccess = false;
+
+  LoginState copyWith({
+    bool? isLoading,
+    bool? isSuccess,
+  }) {
+    return LoginState(
+      isLoading: isLoading ?? this.isLoading,
+      isSuccess: isSuccess ?? this.isSuccess,
+    );
+  }
+}
