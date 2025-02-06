d408a63c02687d63500bc8b38bb058fc9e090436
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-02-06 14:42:13 +0545

    repomock

---
+++ test/features/auth/domain/use_case/repository.mock.dart
@@ -0,0 +1,7 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:rentify_flat_management/features/auth/domain/repository/auth_repository.dart';
+
+
+class MockAuthRepository extends Mock implements IAuthRepository{
+  
+}
