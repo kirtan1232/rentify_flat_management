1d00ed8da68fc714078f1b0e6f5112d332f5bb9b
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-03-05 18:51:27 +0545

    bloc

--- lib/features/auth/presentation/view_model/signup/signup_bloc.dart
+++ lib/features/auth/presentation/view_model/signup/signup_bloc.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
 
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rentify_flat_management/app/di/di.dart'; // Add for getIt
 import 'package:rentify_flat_management/core/common/snackbar/my_snackbar.dart';
 import 'package:rentify_flat_management/features/auth/domain/use_case/signup_usecase.dart';
 import 'package:rentify_flat_management/features/auth/domain/use_case/uploadImage_usecase.dart';
+import 'package:rentify_flat_management/features/auth/presentation/view/login_view.dart'; // Add for LoginScreenView
+import 'package:rentify_flat_management/features/auth/presentation/view_model/login/login_bloc.dart'; // Add for LoginBloc
 
 part 'signup_event.dart';
 part 'signup_state.dart';
@@ -37,11 +40,34 @@ class SignupBloc extends Bloc<SignupEvent, SignupState> {
     ));
 
     result.fold(
-      (l) => emit(state.copyWith(isLoading: false, isSuccess: false)),
+      (failure) {
+        emit(state.copyWith(isLoading: false, isSuccess: false));
+        final message = failure.message.contains("User already exists")
+            ? "Email already exists"
+            : "Signup failed: ${failure.message}";
+        showMySnackBar(
+          context: event.context,
+          message: message,
+          color: Colors.red,
+        );
+      },
       (r) {
         emit(state.copyWith(isLoading: false, isSuccess: true));
         showMySnackBar(
-            context: event.context, message: "Registration Successful");
+          context: event.context,
+          message: "Registration Successful",
+          color: Colors.green,
+        );
+        // Navigate to LoginScreenView after success
+        Navigator.pushReplacement(
+          event.context,
+          MaterialPageRoute(
+            builder: (context) => BlocProvider<LoginBloc>(
+              create: (_) => getIt<LoginBloc>(),
+              child: LoginScreenView(),
+            ),
+          ),
+        );
       },
     );
   }
