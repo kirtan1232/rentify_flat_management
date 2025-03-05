72be06be1ee00a8ff7afd4a107c100d6b3b81c96
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-03-05 18:57:33 +0545

    room_repo

--- lib/features/home/data/repository/room_repository_impl.dart
+++ lib/features/home/data/repository/room_repository_impl.dart
@@ -1,21 +1,30 @@
 import 'package:dartz/dartz.dart';
 import 'package:rentify_flat_management/core/error/failure.dart';
+import 'package:rentify_flat_management/features/home/data/data_source/local_data_source/room_local_data_source.dart';
 import 'package:rentify_flat_management/features/home/data/data_source/remote_data_source/room_remote_data_source.dart';
 import 'package:rentify_flat_management/features/home/domain/entity/room_entity.dart';
 import 'package:rentify_flat_management/features/home/domain/repository/room_repository.dart';
 
-
 class RoomRepositoryImpl implements RoomRepository {
   final RoomRemoteDataSource remoteDataSource;
+  final RoomLocalDataSource localDataSource;
 
-  RoomRepositoryImpl(this.remoteDataSource);
+  RoomRepositoryImpl(this.remoteDataSource, this.localDataSource);
 
   @override
   Future<Either<Failure, List<RoomEntity>>> getAllRooms() async {
     try {
+      // Attempt to fetch from remote
       final roomModels = await remoteDataSource.getAllRooms();
+      // Save to local storage
+      await localDataSource.saveRooms(roomModels);
       return Right(roomModels.map((model) => model.toEntity()).toList());
     } catch (e) {
+      // If remote fetch fails, fall back to local data
+      final localRooms = await localDataSource.getAllRooms();
+      if (localRooms.isNotEmpty) {
+        return Right(localRooms.map((model) => model.toEntity()).toList());
+      }
       return Left(ApiFailure(message: e.toString()));
     }
   }
@@ -34,7 +43,8 @@ class RoomRepositoryImpl implements RoomRepository {
   Future<Either<Failure, List<RoomEntity>>> getWishlist() async {
     try {
       final roomModels = await remoteDataSource.getWishlist();
-      return Right(roomModels.map((model) => model.toEntity(isWishlisted: true)).toList());
+      return Right(
+          roomModels.map((model) => model.toEntity(isWishlisted: true)).toList());
     } catch (e) {
       return Left(ApiFailure(message: e.toString()));
     }
