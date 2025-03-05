db5757e78fd2800ca31eabfe9b404e23b517e811
Kirtan Shrestha <shresthakirtan4@gmail.com>
2025-03-05 18:51:06 +0545

    repository

--- lib/features/auth/data/repository/auth_remote_repository.dart
+++ lib/features/auth/data/repository/auth_remote_repository.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:dartz/dartz.dart';
 import 'package:rentify_flat_management/core/error/failure.dart';
 import 'package:rentify_flat_management/features/auth/data/data_source/remote_data_source/auth_remote_datasource.dart';
@@ -12,19 +11,21 @@ class AuthRemoteRepository implements IAuthRepository {
   AuthRemoteRepository(this._authRemoteDataSource);
 
   @override
-  Future<Either<Failure, AuthEntity>> getCurrentUser() {
-    throw UnimplementedError();
+  Future<Either<Failure, AuthEntity>> getCurrentUser() async {
+    try {
+      final user = await _authRemoteDataSource.getCurrentUser();
+      return Right(user);
+    } catch (e) {
+      return Left(ApiFailure(message: e.toString()));
+    }
   }
 
   @override
-  Future<Either<Failure, String>> loginUser(
-      String email, String password) async {
+  Future<Either<Failure, String>> loginUser(String email, String password) async {
     try {
-      // Call the remote data source to handle the login API call
       final token = await _authRemoteDataSource.loginUser(email, password);
       return Right(token);
     } catch (e) {
-      // If an error occurs, wrap it in a Failure and return as Left
       return Left(ApiFailure(message: e.toString()));
     }
   }
@@ -44,6 +45,30 @@ class AuthRemoteRepository implements IAuthRepository {
     try {
       final imageName = await _authRemoteDataSource.uploadProfilePicture(file);
       return Right(imageName);
+    } catch (e) {
+      return Left(ApiFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, AuthEntity>> updateUser(String? name, String? password, File? image) async {
+    try {
+      String? imageName;
+      if (image != null) {
+        imageName = await _authRemoteDataSource.uploadProfilePicture(image);
+      }
+      final updatedUser = await _authRemoteDataSource.updateUser(name, password, imageName);
+      return Right(updatedUser);
+    } catch (e) {
+      return Left(ApiFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> deleteUser(String password) async { // New method
+    try {
+      await _authRemoteDataSource.deleteUser(password);
+      return const Right(null);
     } catch (e) {
       return Left(ApiFailure(message: e.toString()));
     }
